0cca0948db2932c0c05aaf4b34b8a08a8d28e5b7
Dewanshu Sisaudiya <sisaudiya.dewan17@gmail.com>
2020-09-07 12:03:10 +0530

    commun ication between com

--- src/js/background.js
+++ src/js/background.js
@@ -1,2 +1,16 @@
 import '../img/icon-128.png'
 import '../img/icon-34.png'
+// alert('hi')
+chrome.runtime.sendMessage(
+  { from: 'background', message: 'Information from webpage.' },
+  function(response) {
+      console.log(response);
+    }
+);
+// chrome.runtime.sendMessage({ from: 'bg', message: 'Information from webpage.' });
+console.log(chrome);
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+    //alert('updated from background');
+    console.log('check');
+    chrome.runtime.sendMessage({ from: 'background', message: 'Information from webpage.' });
+});
--- src/js/myContentScript.js
+++ src/js/myContentScript.js
@@ -1,3 +1,10 @@
 console.log("watching on youtube");
-ytplayer = document.getElementById("movie_player");
-ytplayer.getCurrentTime();
+// ytplayer = document.getElementById("movie_player");
+// ytplayer.getCurrentTime();
+console.log(chrome);
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+alert('message')
+  if (request.from == 'background') {
+    console.log('The request has been received from the bg script.');
+  }
+});
